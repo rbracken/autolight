328a81535ea3acfb40259fe7464ef25467690c65
Ryan Brackenbury <ryan.brackenbury@gmail.com>
2016-11-22 23:00:09 -0500

    Wrote install into makefile. Delux now compiles. Also fixed permissions.

--- Makefile
+++ Makefile
@@ -1,4 +1,16 @@
 # For building the delux binary
 all: 
 	gcc -O3 delux.c -o delux
-
+	chmod 755 delux
+	echo "Compilation done. Run \`make install\` to install delux"
+install:
+	echo "This operation requires root privileges"
+	mkdir -p /etc/delux
+	cp delux delux.legacy /usr/sbin/delux
+	cp luxtab.csv /etc/delux/
+	cp init.d/deluxd /etc/init.d/
+	chown -R root: /etc/delux
+	chown root: /etc/init.d/deluxd
+	chown root: /usr/sbin/delux /usr/sbin/delux.legacy
+clean:
+	rm -f delux
--- delux.c
+++ delux.c
@@ -3,39 +3,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main() {
-    //char screenpath[] = "/sys/class/backlight/$SCREEN/brightness";
-    //char sensorpath[] = "/sys/bus/iio/devices/iio\:device0/in_illuminance0_input";
-    char luxtabpath[] = "/home/ryan/delux/luxtab.csv"
-    int **luxtab = NULL;
-    int maxstep = 10;
-    
-    //luxtab -- read in table of brightness values
-    FILE *fp;
-    if(fp = fopen(filepath, "r")) {
-        luxtab = parse_luxtab(fp);
-        fclose(fp);
-    } else {
-        printf("Can't open luxtab!");
-        return 1;
+// Some global variables
+char screenpath[] = "/sys/class/backlight/intel_backlight/brightness";          // This only works on Intel GPU laptops
+char sensorpath[] = "/sys/bus/iio/devices/iio\:device0/in_illuminance0_input";  // Known to work on Acer C720 -- other laptops might have different sensor paths
+char luxtabpath[] = "/etc/delux/luxtab.csv";
 
-    while ( 1 ) {
-        // Read current ambient brightness  
-        int ambl = get_ambient(sensorpath); 
-        if(ambl == -1) {
-            return 1;
-        }
-        // Calc new brightness
-        
-        // Write new brightness out 
-        if(upd_brightness(filepath, brightness)) {
-            return 1;
-        }
-        usleep(10000);
-    }
-
-    return 0;
-}
 
 int upd_brightness( char *filepath, int brightness ) {
     FILE *fp;
@@ -51,11 +23,10 @@ int upd_brightness( char *filepath, int brightness ) {
 
 int get_ambient(char *filepath) {
     FILE *fp;
-
     if(fp = fopen(filepath, "r")) {
         char buffer[64];
         fread(buffer, sizeof(char), sizeof(buffer), fp);
-        val = atoi(buffer);
+        int val = atoi(buffer);
         fclose(fp);
         return val;   
     }
@@ -67,17 +38,17 @@ int get_ambient(char *filepath) {
 int **parse_luxtab(FILE *fp) {
     // Read the luxtab into memory 
     char buffer[2048];
-    int len = int(sizeof(fread(buffer, sizeof(char), sizeof(buffer), fp)));
-    int lines = 0;
+    int len = (int)(sizeof(fread(buffer, sizeof(char), sizeof(buffer), fp)));
+    int lines = 0, count, i;
 
     for(i=0;i++;i<len) {
         if(buffer[i] == '\n') {
-            lines++:
+            lines++;
         }
     }
     int **luxtab = malloc((count-1)*sizeof(int*));
-    count=0;
-    curline=0;
+    count=0; 
+    int curline=0;
     char line[64];
     // TODO reimplement ALL this with strtok 
     for(i=0;i++;i<len) {
@@ -97,5 +68,40 @@ int **parse_luxtab(FILE *fp) {
             curline++;
         }
     }
-    return luxtab
+    return luxtab;
 }
+
+int main() {
+    int **luxtab = NULL;
+    int maxstep = 10;
+    int brightness = 10; // Placeholder value (for now!)
+    
+    //luxtab -- read in table of brightness values
+    FILE *fp;
+    if(fp = fopen(luxtabpath, "r")) {
+        luxtab = parse_luxtab(fp);
+        fclose(fp);
+    } 
+    else {
+        printf("Can't open luxtab!");
+        return 1;
+    }
+    while ( 1 ) {
+        // Read current ambient brightness  
+        int ambl = get_ambient(sensorpath); 
+        if(ambl == -1) {
+            return 1;
+        }
+        // Calc new brightness
+        
+        // Write new brightness out 
+        if(upd_brightness(screenpath, brightness)) {
+            return 1;
+        }
+        usleep(10000);
+    }
+
+    return 0;
+}
+
+
