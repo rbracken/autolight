bccfb461718e8ce82d6d702386915b0c47adfd10
Ryan Brackenbury <ryan.brackenbury@gmail.com>
2017-04-03 11:02:02 -0400

    Updated default paths

--- paths.h
+++ paths.h
@@ -5,10 +5,17 @@
 
 // Definitions used if we cannot automatically find devices
 #if !defined(UDEV)
-    //These two work only on Intel GPU laptops
+    // These two work only on Intel GPU laptops
     #define SCRNPATH "/sys/class/backlight/intel_backlight/brightness"
     #define MAXBRIGHTDEV "/sys/class/backlight/intel_backlight/max_brightness"
+
+    // Default pseudo-device ambient light sensor
+    // Echo values to this path to emulate a sensor
+    #define SENSPATH "/dev/pals"
+
     // Known to work on Acer C720 -- other laptops might have different sensor paths
-    #define SENSPATH "/sys/bus/iio/devices/iio:device0/in_illuminance0_input"
+    // Uncomment this if all else fails and you just want your C720 to work 
+    // #define SENSPATH "/sys/bus/iio/devices/iio:device0/in_illuminance0_input"
+
 #endif
 
