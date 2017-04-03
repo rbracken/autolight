44cd5231b5070954f7e8d855fcaab8763ea4ee8a
Ryan Brackenbury <ryan.brackenbury@gmail.com>
2017-04-03 11:19:30 -0400

    Updated makefile and autoconf

--- paths.h
+++ paths.h
@@ -4,14 +4,21 @@
 #define LUXTAB "/etc/delux/luxtab.csv"
 
 // Definitions used if we cannot automatically find devices
-#if !defined(UDEV)
+#if UDEV == 0
+    
     // These two work only on Intel GPU laptops
+    #if !defined(SCRNPATH)
     #define SCRNPATH "/sys/class/backlight/intel_backlight/brightness"
+    #endif
+    #if !defined(MAXBRIGHTDEV)
     #define MAXBRIGHTDEV "/sys/class/backlight/intel_backlight/max_brightness"
+    #endif
 
     // Default pseudo-device ambient light sensor
     // Echo values to this path to emulate a sensor
+    #if !defined(SENSPATH)
     #define SENSPATH "/dev/pals"
+    #endif
 
     // Known to work on Acer C720 -- other laptops might have different sensor paths
     // Uncomment this if all else fails and you just want your C720 to work 
